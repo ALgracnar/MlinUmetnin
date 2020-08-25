a57fc37e3b5e2300bb37a14183a29bfc699136b2
ALgracnar <algracnar@gmail.com>
2020-08-25 20:05:51 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+###where flask app will be generated
+
+from flask import Flask, render_template
+import logging.config
+import smtplib
+
+
+
+app= Flask(__name__,static_url_path="",static_folder="static")
+app.config['SERVER_NAME'] = 'localhost:888'
+
+@app.route('/')
+def index():
+
+    title='Mlin Umetnin'
+    return render_template('index.html', title=title)
+
+#@app.route('/contact')
+#def contact():
+#    title = 'Mlin Umetnin-Contact'
+#    return render_template('contact.html', title=title)
+
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
